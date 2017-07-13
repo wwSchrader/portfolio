9b35744c4d1c45270c2c7f180b43e5e73303ce03
wwSchrader <wwschrader@gmail.com>
2017-07-12 23:33:17 -0700

    Move NavBar into seperate component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 import particlesParams from './assets/particles.json';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import NavigationBar from './NavigationBar.js';
 
 class App extends Component {
   constructor(props) {
@@ -21,18 +21,7 @@ class App extends Component {
     return (
       <div className="App">
         <Particles id="particles" params={this.particlesParams} style={particleStyle}/>
-        <Navbar default fluid>
-          <Navbar.Header>
-            <Navbar.Brand>Warren Schrader</Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href='#'>Home</NavItem>
-            <NavItem eventKey={2} href='#'>About</NavItem>
-            <NavItem eventKey={3} href='#'>Skills</NavItem>
-            <NavItem eventKey={4} href='#'>Projects</NavItem>
-            <NavItem eventKey={5} href='#'>Contact</NavItem>
-          </Nav>
-        </Navbar>
+        <NavigationBar />
       </div>
     );
   }
