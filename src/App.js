c5a355551b0fa6dd87554cdb8c3089daf0e61a6c
wwSchrader <wwschrader@gmail.com>
2017-07-12 22:29:26 -0700

    Add Navbar with nav items to the major components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Particles from 'react-particles-js';
 import particlesParams from './assets/particles.json';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -21,8 +21,18 @@ class App extends Component {
     return (
       <div className="App">
         <Particles id="particles" params={this.particlesParams} style={particleStyle}/>
-
-
+        <Navbar default fluid>
+          <Navbar.Header>
+            <Navbar.Brand>Warren Schrader</Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href='#'>Home</NavItem>
+            <NavItem eventKey={2} href='#'>About</NavItem>
+            <NavItem eventKey={3} href='#'>Skills</NavItem>
+            <NavItem eventKey={4} href='#'>Projects</NavItem>
+            <NavItem eventKey={5} href='#'>Contact</NavItem>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
