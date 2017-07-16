4ee24741522608ebe280520b18b98145f56824c9
wwSchrader <wwschrader@gmail.com>
2017-07-15 21:35:22 -0700

    Add grid element to contact page

--- src/ContactPageBody.js
+++ src/ContactPageBody.js
@@ -1,44 +1,45 @@
 import React, { Component } from 'react';
-import { FormGroup, ControlLabel, FormControl, Button, Col} from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, Button, Col, Grid} from 'react-bootstrap';
 
 class ContactPageBody extends Component {
 
     render() {
         return (
-          <Col md={6} mdOffset={3} fluid={true}>
-            <form action="https://formspree.io/wwschrader@gmail.com"
-            method="POST">
-                <FormGroup>
-                    <ControlLabel>Name</ControlLabel>
-                    <FormControl
-                        id="formControlsName"
-                        type="text"
-                        label="Name"
-                        name="Name"
-                        placeholder="Enter name"/>
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>E-mail</ControlLabel>
-                    <FormControl
-                        id="formControlsEmail"
-                        type="email"
-                        label="Email address"
-                        name="E-mail"
-                        placeholder="Enter email"/>
-                </FormGroup>
-                <FormGroup>
-                    <ControlLabel>Message</ControlLabel>
-                    <FormControl
-                        id="formControlsMessage"
-                        componentClass="textarea"
-                        label="message"
-                        name="Message"
-                        placeholder="Write message here..."/>
-                </FormGroup>
-                <Button bsStyle="primary" type="submit">Submit</Button>
-            </form>
-
-          </Col>
+            <Grid fluid={true}>
+                <Col md={6} mdOffset={3}>
+                    <form action="https://formspree.io/wwschrader@gmail.com"
+                    method="POST">
+                        <FormGroup>
+                            <ControlLabel>Name</ControlLabel>
+                            <FormControl
+                                id="formControlsName"
+                                type="text"
+                                label="Name"
+                                name="Name"
+                                placeholder="Enter name"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>E-mail</ControlLabel>
+                            <FormControl
+                                id="formControlsEmail"
+                                type="email"
+                                label="Email address"
+                                name="E-mail"
+                                placeholder="Enter email"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>Message</ControlLabel>
+                            <FormControl
+                                id="formControlsMessage"
+                                componentClass="textarea"
+                                label="message"
+                                name="Message"
+                                placeholder="Write message here..."/>
+                        </FormGroup>
+                        <Button bsStyle="primary" type="submit">Submit</Button>
+                    </form>
+                </Col>
+            </Grid>
         );
     }
 }
