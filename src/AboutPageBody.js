b730f8d20e29a64f7d37be31f84f63eb37924ad2
wwSchrader <wwschrader@gmail.com>
2017-07-18 17:54:40 -0700

    Add content to About page

--- src/AboutPageBody.js
+++ src/AboutPageBody.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import { Jumbotron} from 'react-bootstrap';
-import TypeWriter from 'react-typewriter';
+import { Image, Col, Panel} from 'react-bootstrap';
+import portraitImg from './assets/portrait.jpg';
 
 class AboutPageBody extends Component {
 
     render() {
         return (
-          <Jumbotron>
-            <h3>This is the about page</h3>
-          </Jumbotron>
+            <div>
+                <Col xs={6} md={4}>
+                    <Image src={portraitImg} alt="Portrait of Warren Schrader" responsive rounded/>
+                </Col>
+                <Col xs={6} md={4}>
+                    <Panel>
+                        <h1>About Me</h1>
+                        <p>
+                        I'm a newly minted front end web developer. My previous profession was
+                        accounting. However, I couldn't resist the allure of the
+                        fun and challenges in a software development career!
+                        </p>
+
+                        <p>
+                        My focus have been working with Angular and React.
+                        I'll soon be learning Node MongoDB and Express in the near future.
+                        Stay tuned!
+                        </p>
+                    </Panel>
+                </Col>
+            </div>
         );
     }
 }
