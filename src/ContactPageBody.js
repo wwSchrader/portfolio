515db53276f5909993f7cda8e3d8659199b91304
wwSchrader <wwschrader@gmail.com>
2017-08-23 21:29:41 -0700

    Change tabbing to 2 spaces on ContactPageBody

--- src/ContactPageBody.js
+++ src/ContactPageBody.js
@@ -1,49 +1,48 @@
 import React, {Component} from 'react';
 import {FormGroup, ControlLabel, FormControl, Button, Col, Grid}
-        from 'react-bootstrap';
+  from 'react-bootstrap';
 
 class ContactPageBody extends Component {
-
-    render() {
-        return (
-            <Grid fluid>
-                <Col md={6} mdOffset={3}>
-                    <form action="https://formspree.io/wwschrader@gmail.com"
-                        method="POST"
-                    >
-                        <FormGroup controlId='name'>
-                            <ControlLabel>Name</ControlLabel>
-                            <FormControl
-                                type="text"
-                                label="Name"
-                                name="Name"
-                                placeholder="Enter name"
-                            />
-                        </FormGroup>
-                        <FormGroup controlId='e-mail'>
-                            <ControlLabel>E-mail</ControlLabel>
-                            <FormControl
-                                type="email"
-                                label="Email address"
-                                name="E-mail"
-                                placeholder="Enter email"
-                            />
-                        </FormGroup>
-                        <FormGroup controlId='message'>
-                            <ControlLabel>Message</ControlLabel>
-                            <FormControl
-                                componentClass="textarea"
-                                label="message"
-                                name="Message"
-                                placeholder="Write message here..."
-                            />
-                        </FormGroup>
-                        <Button bsStyle="primary" type="submit">Submit</Button>
-                    </form>
-                </Col>
-            </Grid>
-        );
-    }
+  render() {
+    return (
+      <Grid fluid>
+        <Col md={6} mdOffset={3}>
+          <form action="https://formspree.io/wwschrader@gmail.com"
+              method="POST"
+          >
+            <FormGroup controlId='name'>
+              <ControlLabel>Name</ControlLabel>
+              <FormControl
+                  type="text"
+                  label="Name"
+                  name="Name"
+                  placeholder="Enter name"
+              />
+            </FormGroup>
+            <FormGroup controlId='e-mail'>
+              <ControlLabel>E-mail</ControlLabel>
+              <FormControl
+                  type="email"
+                  label="Email address"
+                  name="E-mail"
+                  placeholder="Enter email"
+              />
+            </FormGroup>
+            <FormGroup controlId='message'>
+              <ControlLabel>Message</ControlLabel>
+              <FormControl
+                  componentClass="textarea"
+                  label="message"
+                  name="Message"
+                  placeholder="Write message here..."
+              />
+            </FormGroup>
+            <Button bsStyle="primary" type="submit">Submit</Button>
+          </form>
+        </Col>
+      </Grid>
+    );
+  }
 }
 
 export default ContactPageBody;
