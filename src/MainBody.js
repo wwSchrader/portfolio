30b7c7d15059960f73cff2e85576fcabac29be76
wwSchrader <wwschrader@gmail.com>
2017-08-23 20:59:49 -0700

    Update code format according to eslint on MainBody

--- src/MainBody.js
+++ src/MainBody.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HomePageBody from './HomePageBody.js';
 import AboutPageBody from './AboutPageBody.js';
 import ProjectsPageBody from './ProjectsPageBody.js';
@@ -8,29 +8,29 @@ class MainBody extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentState: props.currentState
+      currentState: props.currentState,
     };
   }
 
   componentWillReceiveProps(nextProps) {
-        this.setState({
-            currentState: nextProps.currentState
-        });
-    }
+    this.setState({
+        currentState: nextProps.currentState,
+    });
+  }
 
-    render() {
-        switch (this.state.currentState) {
-          case 'Contact':
-            return (<ContactPageBody />);
-          case 'Projects':
-            return (<ProjectsPageBody />);
-          case 'About':
-            return (<AboutPageBody />);
-          case 'Home':
-          default:
-            return (<HomePageBody />);
-        }
+  render() {
+    switch (this.state.currentState) {
+      case 'Contact':
+        return <ContactPageBody />;
+      case 'Projects':
+        return <ProjectsPageBody />;
+      case 'About':
+        return <AboutPageBody />;
+      case 'Home':
+      default:
+        return <HomePageBody />;
     }
+  }
 }
 
 export default MainBody;
