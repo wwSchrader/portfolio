7c3a38efcc2b1202187023977e3a7c0d8cf25f78
wwSchrader <wwschrader@gmail.com>
2017-08-23 20:56:25 -0700

    Reformat code accordingt to eslint on HomePageBody

--- src/HomePageBody.js
+++ src/HomePageBody.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { Jumbotron} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Jumbotron} from 'react-bootstrap';
 import TypeWriter from 'react-typewriter';
 
 class HomePageBody extends Component {
 
-    render() {
-        return (
-          <Jumbotron>
-            <h1> Hello. </h1>
-            <TypeWriter typing={1}>
-              <h3>My name is Warren.</h3>
-              <p>A programmer, designer, and creator.</p>
-            </TypeWriter>
-          </Jumbotron>
-        );
-    }
+  render() {
+    return (
+      <Jumbotron>
+        <h1> Hello. </h1>
+        <TypeWriter typing={1}>
+          <h3>My name is Warren.</h3>
+          <p>A programmer, designer, and creator.</p>
+        </TypeWriter>
+      </Jumbotron>
+    );
+  }
 }
 
 export default HomePageBody;
